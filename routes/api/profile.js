e56b9dbc90e6f2f524f7348a5d5a921e0e3a5ac2
faithronnels <faithronnels@yahoo.com>
2019-10-16 10:02:03 +0100

    prepared for deployment

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
+const request = require('request');
+const config = require('config');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 //@route    Get api/profile/me
 //@desc     Get current users profile
@@ -117,7 +120,7 @@ router.get('/user/:user_id', async (req, res) => {
         res.json(profile);
     } catch (err) {
         console.error(err.message);
-        if (err.kind == "ObjectId") {
+        if (err.kind === "ObjectId") {
            return res.status(400).json({msg: "Profile not found"});
         }
         res.status(500).send('Server Error');
@@ -132,6 +135,7 @@ router.get('/user/:user_id', async (req, res) => {
 router.delete('/',auth, async (req, res) => {
     try {
         //@todo - remove users posts
+        await Post.deleteMany({ user: req.user.id });
 
         //Remove profile
          await Profile.findOneAndRemove({ user: req.user.id });
@@ -140,7 +144,7 @@ router.delete('/',auth, async (req, res) => {
         res.json({msg: 'User deleted'});
     } catch (err) {
         console.error(err.message);
-        if (err.kind == "ObjectId") {
+        if (err.kind === "ObjectId") {
            return res.status(400).json({msg: "Profile not found"});
         }
         res.status(500).send('Server Error');
@@ -280,9 +284,34 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
         await profile.save();
         res.json(profile);
     } catch (err) {
-            console.error(err.message);
-            res.status(500).send('Server Error');
-        }
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+//@route    Get education from profile
+//@desc     Get user repos from github
+//@access   Public
+router.get('/github/:username', (req, res) => {
+    try {
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+            method: "GET",
+            headers: { 'user-agent': 'node.js' }
+        };
+        request(options, (error, response, body) => {
+            if (error) console.error(error);
+            if (response.statusCode !== 200) {
+                return res.status(404).json({ msg: "No Github Profile found" });
+            }
+                res.json(JSON.parse(body));
+            
+            })
+    
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+    }
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require("./config/db");
+const path = require('path');
 
 
 
@@ -15,6 +16,15 @@ app.get('/', (req, res) => res.send('API RUNNING'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+//servce static assests in production
+if (process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
